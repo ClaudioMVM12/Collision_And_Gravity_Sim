a0f0de9f0da41b0a4f01e461ead286163bba753d
ClaudioMVM12 <claumvmoreira@gmail.com>
2024-03-09 23:51:31 +0000

    Adicionar arquivos de projeto.

---
+++ Collision_And_Gravity_Sim.h
@@ -0,0 +1,8 @@
+﻿// Collision_And_Gravity_Sim.h: arquivo de inclusão para arquivos de inclusão padrão do sistema,
+// ou arquivos de inclusão específicos a um projeto.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Referencie os cabeçalhos adicionais de que seu programa precisa aqui.
